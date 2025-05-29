277b6894d4ed9e7fa42ca2bdcbff167840b063a4
Shaikh Abdulrauf Asifparvez <arauf0801@gmail.com>
2025-05-29 23:10:56 +0530

    added all host

--- WorkWhiz/settings.py
+++ WorkWhiz/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG','False')=='True'
 
-ALLOWED_HOSTS = ['workwhiz.onrender.com,127.0.0.1,localhost']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [
