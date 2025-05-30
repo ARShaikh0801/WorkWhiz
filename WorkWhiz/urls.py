92d536c54b4ddf82e7f9239da14dbc68a0a09148
Shaikh Abdulrauf Asifparvez <arauf0801@gmail.com>
2025-05-30 09:35:39 +0530

    update DEBUG stting

--- WorkWhiz/urls.py
+++ WorkWhiz/urls.py
@@ -26,7 +26,10 @@ urlpatterns = [
 
 # Serve static and media files during development
 if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+else:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
