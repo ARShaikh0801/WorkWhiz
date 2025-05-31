f1d51397bbcf56ce432eece139c7e6e35c745091
Shaikh Abdulrauf Asifparvez <arauf0801@gmail.com>
2025-05-31 12:46:59 +0530

    hide smtp credentials

--- WorkWhiz/settings.py
+++ WorkWhiz/settings.py
@@ -29,9 +29,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'workwhiz123@gmail.com'
-EMAIL_HOST_PASSWORD = 'bdzp hifb kgpi pbjs'
-DEFAULT_FROM_EMAIL = 'workwhiz123@gmail.com'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='')
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
